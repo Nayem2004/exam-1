568fa70ef4090db023a9fd9c2a302b837f787fca
Nayem2004 <nayemshifat25@gmail.com>
2024-10-01 23:19:00 -0400

    Add files via upload

---
+++ #exam 1.py
@@ -0,0 +1,12 @@
+#exam 1
+for num in range(1, 50): 
+    if num % 2 == 0:
+        print(f"{num}: HiTwo")  
+    if num % 5 == 0:
+        print(f"{num}: HiFive") 
+    if num % 3 == 0 and num % 7 == 0:
+        print(f"{num}: HiThreeorSeven")  
+
+
+
+    
